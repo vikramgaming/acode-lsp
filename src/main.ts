1892038c81fcf04efd68226fa5f427a61b9cf2ec
Raunak Raj <71929976+bajrangCoder@users.noreply.github.com>
2026-01-17 17:54:29 +0530

    fix: proper typing

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,38 @@
-import type { WCPage } from "acode/editor/page";
-import plugin from '../plugin.json';
+import plugin from "../plugin.json";
 
 class AcodePlugin {
-    public baseUrl: string | undefined;
+	public baseUrl: string | undefined;
 
-    async init($page: WCPage, cacheFile: any, cacheFileUrl: string): Promise<void> {
-        // Add your initialization code here
-    }
+	async init(
+		$page: Acode.WCPage,
+		cacheFile: Acode.FileSystem,
+		cacheFileUrl: string,
+	): Promise<void> {
+		// Add your initialization code here
+	}
 
-    async destroy() {
-        // Add your cleanup code here
-    }
+	async destroy() {
+		// Add your cleanup code here
+	}
 }
 
-if(window.acode) {
-    const acodePlugin = new AcodePlugin();
-    acode.setPluginInit(plugin.id, async (baseUrl: string, $page: WCPage, { cacheFileUrl, cacheFile }: any) => {
-        if(!baseUrl.endsWith('/')) {
-            baseUrl += '/';
-        }
-        acodePlugin.baseUrl = baseUrl;
-        await acodePlugin.init($page, cacheFile, cacheFileUrl);
-    });
-    acode.setPluginUnmount(plugin.id, () => {
-        acodePlugin.destroy();
-    });
+if (window.acode) {
+	const acodePlugin = new AcodePlugin();
+	acode.setPluginInit(
+		plugin.id,
+		async (
+			baseUrl: string,
+			$page: Acode.WCPage,
+			{ cacheFileUrl, cacheFile }: Acode.PluginInitOptions,
+		) => {
+			if (!baseUrl.endsWith("/")) {
+				baseUrl += "/";
+			}
+			acodePlugin.baseUrl = baseUrl;
+			await acodePlugin.init($page, cacheFile, cacheFileUrl);
+		},
+	);
+	acode.setPluginUnmount(plugin.id, () => {
+		acodePlugin.destroy();
+	});
 }
